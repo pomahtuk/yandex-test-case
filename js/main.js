4e62f774314ad2d27558bb932e817f488c7a2293
Ilja <pman89@ya.ru>
2013-08-14 10:43:04 +0400

    handlebars

--- js/main.js
+++ js/main.js
@@ -5,5 +5,10 @@ $(function() {
     url:'./presentations.json'
   }).done(function(data) {
     console.log(data);
+    var source   = $("#presentation-index-template").html();
+    var template = Handlebars.compile(source);
+    var html     = template(data);
+    console.log(html);
+    $('.step1 .presentations').html(html);
   });
 })
